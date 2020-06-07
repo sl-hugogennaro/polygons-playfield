655132ae7f513f6e27ba4e339abf5a479ef83110
skornous <hugo.gennaro@gmail.com>
2020-06-07 17:56:52 +0200

    Moved todo & wip to readme file

---
+++ README.md
@@ -0,0 +1,11 @@
+# Polygons playfield
+
+## TODO :
+
+1. add wkt/geojson/number[][][] with 1 function ✓
+2. calc bounds from shape
+3. get center from shape
+
+## WIP
+
+2.
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,6 @@ import { center, zoom, geojsonlinestring, wktpolygonWHole } from './modules/map/
 import { makeMap } from './modules/map'
 import { addGeometry } from './modules/geometry'
 
-//? TODO :
-//? 1. add wkt/geojson/number[][][] with 1 function ✓
-//? 2. calc bounds from shape
-//? 3. get center from shape
-
-//! WIP 2.
-
 makeMap('app', center, zoom).then((map) => {
   // raw point
   addGeometry(map, [center[1], center[0]] as number[])
